3cdb1fbf52b2195161c1fd2244234486e40463bb
song <songbing513@163.com>
2016-10-14 13:59:11 +0800

    开始学习Python学习第四版

--- learn1.py
+++ learn1.py
@@ -2,10 +2,38 @@
 # -*- coding: UTF-8 -*-
 
 
+#列表表达式
+squares=[x**2 for x in [1,2,3,4,5]]
+print(squares)
 
-import sys
+#代码语句
+squares1=[]
+for i in [1,2,3,4,5]:
+    squares1.append(i**2)
+print(squares1)
 
-print(sys.platform)
-print(2**100)
-x='span!'
-print(x*8)
+#map 和匿名函数 在Python3中map函数作为iterators展示
+squares2=list(map(lambda x:x**2,[1,2,3,4,5]))
+print(squares2)
+
+
+#Python3中判断字典key只用get方法取消2中的has_key方法
+#或者使用k in d 检查d中是否有键是k
+dict={'name':'tom',
+      'age':25,
+      'job':'IT'
+      }
+
+print(dict)
+
+if dict.get('name'):
+    print("有")
+else:
+    print("没有")
+
+print(dict.items())
+print(dict.keys())
+
+print(dict.get('ceshi'))
+
+print('ceshi' in dict)
