56a64c111bc9167645ea651b35c89eeba2981b1a
song <songbing513@163.com>
2016-10-20 11:19:55 +0800

    开始学习Python学习第四版增加扩展变量方法

---
+++ file_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+# myfile=open('myfile.txt','w')
+# myfile.write('hello text file\n')
+# myfile.write('goodbye text file\n')
+# myfile.close()
+myfilef=open('myfile.txt')
+# print(myfilef.readline())
+# print(myfilef.readline())
+# print(myfilef.readline())
+
+print (myfilef.read())
