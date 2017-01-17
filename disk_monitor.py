d22d6ef3bf99b117c2e7d354f73ae8a7f579702a
songbing <songbing5132163.com>
2017-01-17 16:14:50 +0800

    磁盘报警配置邮件报警为函数

--- disk_monitor.py
+++ disk_monitor.py
@@ -43,8 +43,20 @@ password = "%!#+513-1990bing"
 to_addr = "songbing513@163.com"
 smtp_server = "smtp.163.com"
 
-def disk(disk_status):
 
+def mailsend(mes):
+    msg = MIMEText(mes, 'plain', 'utf-8')
+    msg['From'] = _format_addr(u'报警 <%s>' % from_addr)
+    msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+    msg['Subject'] = Header(u'阿里服务器', 'utf-8').encode()
+    server = smtplib.SMTP(smtp_server, 25)
+    # server.set_debuglevel(1)
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.quit()
+
+
+def disk(disk_status):
     for i in file.readlines():
         reip = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
         if reip.findall(i):
@@ -61,27 +73,11 @@ def disk(disk_status):
 disk_status=disk(disk_status)
 if disk_status=="":
     disk_status="磁盘使用正常"
-    msg = MIMEText(disk_status, 'plain', 'utf-8')
-    msg['From'] = _format_addr(u'磁盘报警 <%s>' % from_addr)
-    msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-    msg['Subject'] = Header(u'阿里服务器', 'utf-8').encode()
-    server = smtplib.SMTP(smtp_server, 25)
-    # server.set_debuglevel(1)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, [to_addr], msg.as_string())
-    server.quit()
-    file.close()
+    mailsend(disk_status)
 else:
-    msg = MIMEText(disk_status, 'plain', 'utf-8')
-    msg['From'] = _format_addr(u'磁盘报警 <%s>' % from_addr)
-    msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-    msg['Subject'] = Header(u'阿里服务器', 'utf-8').encode()
-    server = smtplib.SMTP(smtp_server, 25)
-    # server.set_debuglevel(1)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, [to_addr], msg.as_string())
-    server.quit()
-    file.close()
+    mailsend(disk_status)
+
+file.close()
 
 
 
